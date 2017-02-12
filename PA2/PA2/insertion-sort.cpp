432ea3ea157ae56207d00876b4fe250cbaf8b9b4
Tate Walker <tatewalker@tamu.edu>
2017-02-12 14:16:39 -0600

    setup basic insertion sort code

---
+++ PA2/PA2/insertion-sort.cpp
@@ -0,0 +1,38 @@
+//============================================================================
+// Name        : insertion-sort.cpp
+// Author      : Tate Walker
+// Date        : 2-11-17
+// Copyright   : Tate Walker
+// Description : Implementation of insertion sort in C++
+//============================================================================
+
+#include "sort.h"
+using namespace std;
+
+void
+InsertionSort::sort(int A[], int size)				// main entry point
+{
+    int j;
+    int temp;
+    for (int i = 0; i<size; i++) {
+        j = i;
+        
+        while (j>0 && A[j] < A[j-1]) {
+            temp = A[j];
+            A[j] = A[j-1];
+            A[j-1] = temp;
+            j--;
+        }
+    }
+    
+    
+  /* Complete this function with the implementation of insertion sort algorithm 
+  Record number of comparisons in variable num_cmps of class Sort
+  */
+}
+
+int main(){
+    A = [4,5,3,2,6];
+    
+    
+}
