b07449e930678bea31ddb4399366463b8c1bf89f
salman0907 <salman0907@email.tamu.edu>
2017-02-18 15:12:46 -0600

    Fixed radix sort

--- radix-sort.cpp
+++ radix-sort.cpp
@@ -9,7 +9,6 @@
 #include "sort.h"
 #include <iostream>
 
-
 int getMax(int A[], int size)
 {
     int max = A[0];
@@ -17,6 +16,16 @@ int getMax(int A[], int size)
         if (A[i] > max)
             max = A[i];
     return max;
+}
+
+int getMin(int A[], int size) {
+    int min = A[0];
+    for(int i = 1; i<size; ++i) {
+        if(A[i]<min) {
+            min = A[i];
+        }
+    }
+    return min;
 }
 
 void countSort(int A[], int size, int exp)
@@ -39,7 +48,35 @@ void countSort(int A[], int size, int exp)
 void
 RadixSort::sort(int A[], int size)
 {   
+    //check to see if there are any negative numbers in the input
+    bool anyNegatives = false;
+    for(int i = 0; i<size; ++i) {  
+        if(A[i] < 0) {
+            anyNegatives = true;
+            break;
+        }
+    }
+
+    //if there are any negative numbers, shift the input up by the smallest
+    //this will make all numbers in input >= 0
+    int smallest = getMin(A, size);
+    smallest *= -1;
+    if(anyNegatives) {
+        for(int i = 0; i<size; ++i) {
+            A[i] += smallest;
+        }
+    }
+
+    //sort the non-negative numbers
     int m = getMax(A, size);
     for (int exp = 1; m / exp > 0; exp *= 10)
         countSort(A, size, exp);
+
+    //shift the array back down
+    if(anyNegatives) {
+        for(int i = 0; i<size; ++i) {
+            A[i] -= smallest;
+        }
+    }
+
 }
